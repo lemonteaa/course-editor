8fc49c9b2cce3638a8290d0b8ebd9ba3343ee3bb
Lemontea <lemontea.Tom@gmail.com>
2021-12-26 04:34:18 +0800

    Basic modern integration against BrowserFS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+import * as BrowserFS from 'browserfs';
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { Text, Input, Button } from '@chakra-ui/react'
+
+import MDEditor from '@uiw/react-md-editor';
+
+var bfs = {};
+BrowserFS.install(bfs);
+BrowserFS.configure({
+  fs: "LocalStorage"
+}, function (e) {
+  if (e) {
+    // An error happened!
+    throw e;
+  }
+  // Otherwise, BrowserFS is ready-to-use!
+  console.log("FS Ready");
+})
+
 function App() {
+  const [fileName, setFileName] = useState("");
+  const [fileContent, setFileContent] = useState("**Hello world!!!**");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event) => setFileName(event.target.value)
+
+  const saveFile = () => {
+    setLoading(true);
+    let fs = bfs.require('fs');
+    fs.writeFile(fileName, fileContent, function (e) {
+      //TODO
+      console.log("Write FIle CB")
+      console.log(e);
+      setLoading(false);
+    })
+  }
+
+  const readFile = () => {
+    let fs = bfs.require('fs');
+    fs.readFile(fileName, (err, content) => {
+      //TODO
+      console.log("Read FIle CB")
+      console.log(content.toString())
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChakraProvider>
+        <Text>Course Editor</Text>
+        <MDEditor
+          value={fileContent}
+          onChange={setFileContent}
+        />
+        <Input 
+          value={fileName}
+          onChange={handleChange}
+          placeholder='File Name' 
+        /> <Button isLoading={loading} onClick={saveFile}>Save</Button>
+        <Button onClick={readFile}>Read File (Test)</Button>
+      </ChakraProvider>
     </div>
   );
 }
